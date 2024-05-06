d45fe303baf10921c84e88ff98c6574eeed852ca
Jasmijntjuhhhhhh <88290559+Jasmijntjuhhhhhh@users.noreply.github.com>
2024-05-06 20:30:40 +0200

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Update(){
+        if (Input.GetKey(KeyCode.W)) {
+        }
+        if (Input.GetKey(KeyCode.S)){
+        }
+        if (Input.GetKey(KeyCode.A)) {
+        }
+        if (Input.GetKey(KeyCode.D)) {
+        }
+    }
+}
